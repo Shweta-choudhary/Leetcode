b640b23d8f2ad78dfe95a3b2a19e4b4c89a54cbd
Shweta-choudhary <91624353+Shweta-choudhary@users.noreply.github.com>
2023-06-23 15:09:41 +0530

    Added solution - LeetHub

--- Roman Number to Integer - GFG/roman-number-to-integer.cpp
+++ Roman Number to Integer - GFG/roman-number-to-integer.cpp
@@ -10,30 +10,22 @@ using namespace std;
 
 class Solution {
   public:
-    int romanToDecimal(string &s) {
+    int romanToDecimal(string &str) {
         // code here
-         unordered_map<char, int> T = { { 'I' , 1 },
-                                   { 'V' , 5 },
-                                   { 'X' , 10 },
-                                   { 'L' , 50 },
-                                   { 'C' , 100 },
-                                   { 'D' , 500 },
-                                   { 'M' , 1000 } };
-                                   
-   int sum = T[s.back()];
-   for (int i = s.length() - 2; i >= 0; --i) 
-   {
-       if (T[s[i]] < T[s[i + 1]])
-       {
-           sum -= T[s[i]];
-       }
-       else
-       {
-           sum += T[s[i]];
-       }
-   }
-   
-   return sum;
+        unordered_map<char,int>mp={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
+        
+        int res=mp[str.back()];
+        
+        for(int i=str.size()-2;i>=0;--i)
+        {
+            if(mp[str[i]]<mp[str[i+1]])
+            res-=mp[str[i]];
+            
+            else
+            res+=mp[str[i]];
+        }
+        
+        return res;
     }
 };
 
