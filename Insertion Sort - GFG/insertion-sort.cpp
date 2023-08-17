4eafa80a4d788a772db2eab68f521a8dbe1cffdd
Shweta-choudhary <91624353+Shweta-choudhary@users.noreply.github.com>
2023-08-17 21:15:57 +0530

    Added solution - LeetHub

--- Insertion Sort - GFG/insertion-sort.cpp
+++ Insertion Sort - GFG/insertion-sort.cpp
@@ -33,8 +33,8 @@ class Solution
             
             while(j>=0 && arr[j]>temp)
             {
-                arr[j+1]=arr[j];
-                j--;
+            arr[j+1]=arr[j];
+            j--;
             }
             
             arr[j+1]=temp;
