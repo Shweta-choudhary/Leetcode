b8cac69a824ebc8fefa61e2022955e1d6bb55d6d
Shweta-choudhary <91624353+Shweta-choudhary@users.noreply.github.com>
2023-09-23 23:21:19 +0530

    Added solution - LeetHub

---
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    bool dfs(int i,vector<int> adj[],vector<int>& vis,vector<int> & path,vector<int>& check)
+    {
+        vis[i]=1;
+        path[i]=1;
+        check[i]=0;
+        
+        for(auto it:adj[i])
+        {
+            if(vis[it]==0)
+            {
+                if(dfs(it,adj,vis,path,check)==true)
+                {
+                    check[it]=0;
+                    return true;
+                }
+            }
+            
+            else if(path[it]==1)
+            {
+                check[it]=0;
+                return true;
+            }
+        }
+        
+        
+        check[i]=1;
+        path[i]=0;
+        return false;
+    }
+  
+    vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
+        // code here
+        vector<int>vis(V,0);
+        vector<int>path(V,0);
+        vector<int>check(V,0);
+        vector<int>res;
+        
+        for(int i=0;i<V;i++)
+        {
+            if(vis[i]==0)
+            {
+                dfs(i,adj,vis,path,check);
+            }
+        }
+        
+        for(int i=0;i<V;i++)
+        {
+            if(check[i]==1)
+            res.push_back(i);
+        }
+        
+        return res;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        Solution obj;
+        vector<int> safeNodes = obj.eventualSafeNodes(V, adj);
+        for (auto i : safeNodes) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
