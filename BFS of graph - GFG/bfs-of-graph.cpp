f7854a2f355603e46734ccc5a72af39c47f1a706
Shweta-choudhary <91624353+Shweta-choudhary@users.noreply.github.com>
2023-10-22 18:19:37 +0530

    Added solution - LeetHub

--- BFS of graph - GFG/bfs-of-graph.cpp
+++ BFS of graph - GFG/bfs-of-graph.cpp
@@ -8,8 +8,8 @@ class Solution {
     // Function to return Breadth First Traversal of given graph.
     vector<int> bfsOfGraph(int V, vector<int> adj[]) {
         // Code here
-        vector<int>res;
-        int vis[V]={0};
+        vector<int>bfs;
+        vector<int>vis(V,0);
         queue<int>q;
         q.push(0);
         vis[0]=1;
@@ -18,19 +18,19 @@ class Solution {
         {
             int node=q.front();
             q.pop();
-            res.push_back(node);
+            bfs.push_back(node);
             
             for(auto it:adj[node])
             {
                 if(vis[it]==0)
                 {
-                    vis[it]=1;
                     q.push(it);
+                    vis[it]=1;
                 }
             }
         }
         
-        return res;
+        return bfs;
     }
 };
 
