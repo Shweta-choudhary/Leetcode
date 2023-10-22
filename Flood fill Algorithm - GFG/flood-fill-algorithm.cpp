70d052afea1f34bac6121753528f1c7399881613
Shweta-choudhary <91624353+Shweta-choudhary@users.noreply.github.com>
2023-10-22 20:34:15 +0530

    Added solution - LeetHub

--- Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
@@ -5,44 +5,33 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int sr, int sc, int newColor, vector<vector<int>>& grid, int color) {
-    int n = image.size();
-    int m = image[0].size();
-
-    if (sr < 0 || sr >= n || sc < 0 || sc >= m)
-        return;
-
-    if (image[sr][sc] != color)
+    void dfs(vector<vector<int>>& image,int sr,int sc,int newColor,int original)
+    {
+        int n=image.size();
+        int m=image[0].size();
+        
+        if(sr<0 || sr>=n || sc<0 || sc>=m)
         return;
         
-    if(grid[sr][sc]==newColor)
-    return;
-
-    grid[sr][sc] = newColor;
-
-    dfs(image, sr - 1, sc, newColor, grid, color);
-    dfs(image, sr, sc + 1, newColor, grid, color);
-    dfs(image, sr + 1, sc, newColor, grid, color);
-    dfs(image, sr, sc - 1, newColor, grid, color);
-}
-
-vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-    int n = image.size();
-    int m = image[0].size();
-
-    vector<vector<int>> grid(n, vector<int>(m, 0));
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            grid[i][j] = image[i][j];
-        }
+        if(image[sr][sc]!=original)
+        return;
+        
+        image[sr][sc]=newColor;
+        dfs(image,sr-1,sc,newColor,original);
+        dfs(image,sr,sc+1,newColor,original);
+        dfs(image,sr+1,sc,newColor,original);
+        dfs(image,sr,sc-1,newColor,original);
     }
 
-    dfs(image, sr, sc, newColor, grid, image[sr][sc]);
-
-    return grid;
-}
-
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
+        // Code here 
+        if(image[sr][sc]==newColor)
+        return image;
+        
+        dfs(image,sr,sc,newColor,image[sr][sc]);
+        
+        return image;
+    }
 };
 
 //{ Driver Code Starts.
