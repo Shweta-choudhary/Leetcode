52e6b9ebc403c319af598e47ccb080473663e4ed
Shweta-choudhary <91624353+Shweta-choudhary@users.noreply.github.com>
2023-10-23 12:55:58 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.cpp
@@ -8,54 +8,52 @@ using namespace std;
 // User function Template for C++
 
 class Solution {
-    void func(vector<pair<int,int>>& curr)
+  public:
+    void func(vector<pair<int,int>>&curr)
     {
         pair<int,int>p=curr[0];
         
         for(int i=0;i<curr.size();i++)
         {
-        curr[i].first-=p.first;
-        curr[i].second-=p.second;
+            curr[i].first-=p.first;
+            curr[i].second-=p.second;
         }
     }
-    
-    void dfs(int i,int j,vector<vector<int>>&grid,vector<pair<int,int>>& curr,vector<vector<int>>& vis)
-    {
-        int n=grid.size();
-        int m=grid[0].size();
-        vis[i][j]=1;
-        curr.push_back({i,j});
-        
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
-        
-        for(int i1=0;i1<4;i1++)
-        {
-            int r=i+drow[i1];
-            int c=j+dcol[i1];
-            
-            if(r>=0 && r<n && c>=0 && c<m && grid[r][c]==1 && vis[r][c]==0)
-            dfs(r,c,grid,curr,vis);
-        }
+  
+   void dfs(vector<vector<int>>& grid, vector<pair<int, int>>& curr, int i, int j, vector<vector<int>>& vis) {
+    int n = grid.size();
+    int m = grid[0].size();
+    curr.push_back({ i, j });
+    vis[i][j] = 1;
+
+    int drow[] = { -1, 0, 1, 0 };
+    int dcol[] = { 0, 1, 0, -1 };
+
+    for (int k = 0; k < 4; k++) {
+        int r = i + drow[k];
+        int c = j + dcol[k];
+
+        if (r >= 0 && r < n && c >= 0 && c < m && grid[r][c] == 1 && vis[r][c] == 0)
+            dfs(grid, curr, r, c, vis);
     }
-    
-  public:
+}
+
+  
     int countDistinctIslands(vector<vector<int>>& grid) {
         // code here
         int n=grid.size();
         int m=grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
-        
         set<vector<pair<int,int>>>s;
         
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(vis[i][j]==0 && grid[i][j]==1)
+                if(grid[i][j]==1 && vis[i][j]==0)
                 {
                     vector<pair<int,int>>curr;
-                    dfs(i,j,grid,curr,vis);
+                    dfs(grid,curr,i,j,vis);
                     sort(curr.begin(),curr.end());
                     func(curr);
                     s.insert(curr);
