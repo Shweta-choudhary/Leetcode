54ddb015db8dd0b4bdce2225517f6bd9403ed648
Shweta-choudhary <91624353+Shweta-choudhary@users.noreply.github.com>
2023-11-07 18:03:56 +0530

    Added solution - LeetHub

--- Number Of Enclaves - GFG/number-of-enclaves.cpp
+++ Number Of Enclaves - GFG/number-of-enclaves.cpp
@@ -11,68 +11,55 @@ using namespace std;
 class Solution {
   public:
     int numberOfEnclaves(vector<vector<int>> &grid) {
-        // Code here
-        int n=grid.size();
-        int m=grid[0].size();
-        int original=0;
-        
-        vector<vector<int>>res(n,vector<int>(m,0));
-        
-        queue<pair<int,int>>q;
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(i==0 || i==n-1 || j==0 || j==m-1)
-                {
-                    if(grid[i][j]==1)
-                    {
-                        res[i][j]=1;
-                    q.push({i,j});
-                    }
-                }
-                
-                if(grid[i][j]==1)
-                original++;
+    int n = grid.size();
+    int m = grid[0].size();
+    
+    vector<vector<int>> res(n, vector<int>(m, 0));
+    queue<pair<int, int>> q;
+    
+    // Marking cells connected to the border
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if ((i == 0 || i == n - 1 || j == 0 || j == m - 1) && grid[i][j] == 1) {
+                res[i][j] = 1;
+                q.push({i, j});
             }
         }
+    }
+    
+    // BFS to mark all connected land cells
+    while (!q.empty()) {
+        int row = q.front().first;
+        int col = q.front().second;
+        q.pop();
         
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
         
-        while(q.empty()==0)
-        {
-            int row=q.front().first;
-            int col=q.front().second;
-            q.pop();
+        for (int i = 0; i < 4; i++) {
+            int r = row + drow[i];
+            int c = col + dcol[i];
             
-            for(int i=0;i<4;i++)
-            {
-                int r=row+drow[i];
-                int c=col+dcol[i];
-                
-                if(r>=0 && r<n && c>=0 && c<m && res[r][c]==0 && grid[r][c]==1)
-                {
-                    res[r][c]=1;
-                    q.push({r,c});
-                }
+            if (r >= 0 && r < n && c >= 0 && c < m && grid[r][c] == 1 && res[r][c] == 0) {
+                res[r][c] = 1;
+                q.push({r, c});
             }
         }
-        
-        int count=0;
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(res[i][j]==1)
+    }
+    
+    int count = 0;
+    // Count the number of enclaves
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (grid[i][j] == 1 && res[i][j] == 0) {
                 count++;
             }
         }
-        
-        return original-count;
     }
+    
+    return count;
+}
+
 };
 
 
