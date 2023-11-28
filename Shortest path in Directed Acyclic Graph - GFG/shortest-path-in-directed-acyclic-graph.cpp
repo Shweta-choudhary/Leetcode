af964868393004be8a86c1c341ebe992649e6b5a
Shweta-choudhary <91624353+Shweta-choudhary@users.noreply.github.com>
2023-11-28 18:53:05 +0530

    Added solution - LeetHub

--- Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
+++ Shortest path in Directed Acyclic Graph - GFG/shortest-path-in-directed-acyclic-graph.cpp
@@ -8,20 +8,20 @@ using namespace std;
 // User function Template for C++
 class Solution {
   public:
-    void toposort(int i,vector<pair<int,int>>adj[],int vis[],stack<int>& st)
-    {
-        vis[i]=1;
-        
-        for(auto it:adj[i])
-        {
-            int v=it.first;
-            
-            if(vis[v]==0)
-            toposort(v,adj,vis,st);
-        }
-        
-        st.push(i);
-    }
+     void toposort(int node,vector<pair<int,int>>adj[],vector<int>& vis,stack<int>& st)
+     {
+         vis[node]=1;
+         
+         for(auto it:adj[node])
+         {
+             int v=it.first;
+             
+             if(!vis[v])
+             toposort(v,adj,vis,st);
+         }
+         
+         st.push(node);
+     }
   
      vector<int> shortestPath(int N,int M, vector<vector<int>>& edges){
         // code here
@@ -29,27 +29,28 @@ class Solution {
         
         for(int i=0;i<M;i++)
         {
-            adj[edges[i][0]].push_back({edges[i][1],edges[i][2]});
+            int u=edges[i][0];
+            int v=edges[i][1];
+            int wt=edges[i][2];
+            
+            adj[u].push_back({v,wt});
         }
         
-        int vis[N]={0};
+        vector<int>vis(N,0);
         
         stack<int>st;
         
         for(int i=0;i<N;i++)
         {
-            if(vis[i]==0)
+            if(!vis[i])
             toposort(i,adj,vis,st);
         }
         
-        vector<int>dist(N);
-        
-        for(int i=0;i<N;i++)
-        dist[i]=1e9;
+        vector<int>dist(N,1e9);
         
         dist[0]=0;
         
-        while(st.empty()==0)
+        while(!st.empty())
         {
             int node=st.top();
             st.pop();
@@ -60,7 +61,7 @@ class Solution {
                 int wt=it.second;
                 
                 if(dist[node]+wt<dist[v])
-                dist[v]=dist[node]+wt;
+                dist[v]=wt+dist[node];
             }
         }
         
